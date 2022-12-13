0db04a6e4e97230ec30a9a97417845f75110102e
ZephyrusZhang <82928147+ZephyrusZhang@users.noreply.github.com>
2022-12-14 01:43:14 +0800

    chore: format

--- test/basic_handshaking_test.py
+++ test/basic_handshaking_test.py
@@ -2,6 +2,7 @@ import grader
 import time
 import pytest
 
+
 @pytest.fixture(scope='module')
 def normal_session():
     blocking_time = 10
@@ -23,18 +24,22 @@ def normal_session():
     
     return handshaking_session
 
+
 def test_flooding_whohas(normal_session):
     handshaking_session = normal_session
     for i in range(48002, 48008):
         assert handshaking_session.peer_list[("127.0.0.1", 48001)].send_record[("127.0.0.1", i)][0] > 0, f"Fail to send WHOHAS to {i}"
 
+
 def test_send_ihave(normal_session):
     handshaking_session = normal_session
     assert handshaking_session.peer_list[("127.0.0.1", 48002)].send_record[("127.0.0.1", 48001)][1] > 0, "Fail to send IHAVE"
 
+
 def test_send_download(normal_session):
     handshaking_session = normal_session
     assert handshaking_session.peer_list[("127.0.0.1", 48001)].send_record[("127.0.0.1", 48002)][2] > 0, "Fail to send DOWLOAD"
+
 
 def test_handshaking(normal_session):
     handshaking_session = normal_session
